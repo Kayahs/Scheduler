b0be1c4d5fea4eaf6f170e56d186e6754c61f706
Kayahs F <akshaykmanchanda@gmail.com>
2025-01-10 14:55:03 -0500

    Import Themes and Baselines from mui to build on

--- client/src/components/containers/AppContainer.jsx
+++ client/src/components/containers/AppContainer.jsx
@@ -1,13 +1,23 @@
 import { ApolloProvider } from '@apollo/client'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
 
 import apolloClient from 'config/apolloClient'
 import MainRouter from 'components/MainRouter'
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark'
+  }
+});
 
 const AppContainer = () => {
   return (
-    <ApolloProvider client={apolloClient}>
-      <MainRouter />
-    </ApolloProvider>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <ApolloProvider client={apolloClient}>
+        <MainRouter />
+      </ApolloProvider>
+    </ThemeProvider>
   )
 }
 
